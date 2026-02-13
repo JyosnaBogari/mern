3da08c0f05f0c4b180b35676ac77ea6c2a523e29
Bogari Jyosna <jyosnabogari05@gmail.com>
2026-02-13 20:58:05 +0530

    studentMarksList

--- week1/studentMarksList.js
+++ week1/studentMarksList.js
@@ -7,20 +7,20 @@ Scenario: You receive marks from an exam system.
 const marks = [78, 92, 35, 88, 40, 67];
 
 // filter(): selects marks that are greater than or equal to 40 (pass marks)
-let res1 = marks.filter(ele => ele >= 40);
-console.log(res1); // [78, 92, 88, 40, 67]
+let getMarks = marks.filter(ele => ele >= 40);
+console.log(getMarks); // [78, 92, 88, 40, 67]
 
 // map(): adds 5 grace marks to each student's score
-let res2 = marks.map(ele => ele + 5);
-console.log(res2); // [83, 97, 40, 93, 45, 72]
+let getScores = marks.map(ele => ele + 5);
+console.log(getScores); // [83, 97, 40, 93, 45, 72]
 
 // reduce(): finds the highest mark in the array
-let res3 = marks.reduce((acc, ele) => acc > ele ? acc : ele);
-console.log(res3); // 92
+let highestMark= marks.reduce((acc, ele) => acc > ele ? acc : ele);
+console.log(highestMark); // 92
 
 // find(): finds the first student who failed (marks less than 40)
-let res4 = marks.find(ele => ele < 40);
-console.log(res4); // 35
+let fail = marks.find(ele => ele < 40);
+console.log(fail); // 35
 
 // findIndex(): finds the index position of the mark 92
 let res5 = marks.findIndex(ele => ele == 92);
